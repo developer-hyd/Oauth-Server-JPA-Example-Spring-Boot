ed3ba94f2604929c6e0f780cc58bdffb0711794e
ashish.pushp <ashish.pushp@oildex.com>
2017-07-27 15:30:36 +0530

    Refactored Git project

---
+++ src/main/java/com/oauth/example/services/CustomUserDetailsService.java
@@ -0,0 +1,32 @@
+package com.oauth.example.services;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.oauth.example.entities.CustomUserDetails;
+import com.oauth.example.entities.User;
+import com.oauth.example.repositories.UserRepository;
+
+import java.util.Optional;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Optional<User> optionalUsers = userRepository.findByName(username);
+
+        optionalUsers
+                .orElseThrow(() -> new UsernameNotFoundException("Username not found"));
+        return optionalUsers
+                .map(CustomUserDetails::new).get();
+    }
+}
